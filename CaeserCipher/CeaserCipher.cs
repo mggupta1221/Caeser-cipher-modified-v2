2883d3f4c5413cc02e0edfbb0761b50a388b4030
Mukesh Gupta <68068160+mggupta1221@users.noreply.github.com>
2022-11-01 16:41:43 +0530

    Caeser cipher v2

---
+++ CaeserCipher/CeaserCipher.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace CaeserCipherAlgorithm
+{
+    public class CaeserCipher
+    {
+        private string cipheredText = "";
+
+        private bool IsAlphabet(int charAsciiCheck)
+        {
+            return ((IsCapitalAlphabet(charAsciiCheck) || IsSmallAlphabet(charAsciiCheck)));
+        }
+        private bool IsCapitalAlphabet(int charAsciiCheck)
+        {
+            return (charAsciiCheck >= 65 && charAsciiCheck <= 90);
+        }
+        private bool IsSmallAlphabet(int charAsciiCheck)
+        {
+            return (charAsciiCheck >= 97 && charAsciiCheck <= 122);
+        }
+        public string DoCeaserCipher(string inputString, int shiftingFactor) 
+        {
+
+            for (int count = 0; count < inputString.Length; count++)
+            {
+                int charAsciiValue = (int)(inputString[count]);
+                if (IsAlphabet(charAsciiValue))
+                {
+                    int charAsciiValueWithShiftinFactor = charAsciiValue + shiftingFactor;
+                    if ((IsCapitalAlphabet(charAsciiValueWithShiftinFactor) && IsCapitalAlphabet(charAsciiValue)) || ((IsSmallAlphabet(charAsciiValueWithShiftinFactor) && IsSmallAlphabet(charAsciiValue))))
+                    {
+                        char cipheredCharacter = (char)charAsciiValueWithShiftinFactor;
+                        cipheredText += cipheredCharacter;
+                    }
+                    else
+                    {
+                        if (shiftingFactor > 0)
+                        {
+                            if (IsCapitalAlphabet(charAsciiValue))
+                            {
+                                charAsciiValueWithShiftinFactor = 64 + (charAsciiValueWithShiftinFactor - 90);
+                                char c = (char)charAsciiValueWithShiftinFactor;
+                                cipheredText += c;
+                            }
+                            else if (IsSmallAlphabet(charAsciiValue)) 
+                            {
+                                charAsciiValueWithShiftinFactor = 96 + (charAsciiValueWithShiftinFactor - 122);
+                                char c = (char)charAsciiValueWithShiftinFactor;
+                                cipheredText += c;
+                            }
+                        }
+                        else
+                        {
+                            if (IsCapitalAlphabet(charAsciiValue))
+                            {
+                                if (charAsciiValueWithShiftinFactor < 65)
+                                {
+                                    charAsciiValueWithShiftinFactor = 90 - (64 - charAsciiValueWithShiftinFactor);
+                                }
+                                char c = (char)charAsciiValueWithShiftinFactor;
+                                cipheredText += c;
+                            }
+                            else if (IsSmallAlphabet(charAsciiValue) && charAsciiValueWithShiftinFactor < 97) 
+                            {
+                                charAsciiValueWithShiftinFactor = 122 - (96 - charAsciiValueWithShiftinFactor);
+                                char c = (char)charAsciiValueWithShiftinFactor;
+                                cipheredText += c;
+                            }
+                        }
+
+                    }
+                }
+                else
+                {
+                    cipheredText += inputString[count];
+                }
+            }
+            return cipheredText;
+        }
+    }
+}
---
+++ CaeserCipher/Program.cs
@@ -0,0 +1,29 @@
+﻿using CaeserCipherAlgorithm;
+
+public class Program{
+    public static void Main()
+    {
+        try
+        {
+            Console.Write("Enter text:");
+            string inputString = Console.ReadLine();
+            Console.Write("Enter Shifting factor:");
+            int shiftingFactor = int.Parse(Console.ReadLine());
+            CaeserCipher ceaserCipher = new CaeserCipher();
+
+            string cipheredText = ceaserCipher.DoCeaserCipher(inputString, shiftingFactor);
+            Console.WriteLine("Ciphered Text:" + cipheredText);
+        }
+        catch (FormatException ex)
+        {
+            Console.WriteLine(ex.Message);
+            Console.WriteLine();
+        }
+        catch (NullReferenceException ex1)
+        {
+            Console.WriteLine(ex1.Message);
+            Console.WriteLine();
+        }
+
+    }
+}
